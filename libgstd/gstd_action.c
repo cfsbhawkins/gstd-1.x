288820943024f90c5e804bf45fbbfbb9cb94aa5f
David Monge <david.monge@ridgerun.com>
2025-07-30 15:05:35 -0600

    Add last state of gstd_action.c

--- libgstd/gstd_action.c
+++ libgstd/gstd_action.c
@@ -209,6 +209,8 @@ gstd_action_create_default (GstdObject * object, const gchar * name,
   GSignalQuery query;
   guint action_id;
   gint ret_sig = 0;
+  GValue *args = NULL;
+  gchar **arg_list = NULL;
 
   GST_INFO_OBJECT (action, "Action create");
 
@@ -222,19 +224,71 @@ gstd_action_create_default (GstdObject * object, const gchar * name,
       G_OBJECT_TYPE (action->target));
   g_signal_query (action_id, &query);
 
-  if (query.n_params > 0) {
-    GST_ERROR_OBJECT (action, "Only actions with no parameters are supported");
-    ret = GSTD_BAD_VALUE;
-    goto out;
+  GST_ERROR_OBJECT (action, "description: %s", description);
+
+  // You must parse description into arg_list[] matching query.n_params
+  arg_list = g_strsplit (description, " ", query.n_params + 1);
+  if (!arg_list) {
+    GST_ERROR_OBJECT (action, "!arg_list");
+    return GSTD_BAD_VALUE;
   }
 
-  GST_INFO_OBJECT (action, "Emit to %s", GST_OBJECT_NAME (action->target));
-  g_signal_emit_by_name (action->target, name, &ret_sig);
+  args = g_new0 (GValue, query.n_params + 2);
 
-  if (ret_sig != 0) {
-    ret = GSTD_BAD_VALUE;
+  g_value_init (&args[0], G_TYPE_OBJECT);
+  g_value_set_object (&args[0], action->target);
+
+  for (guint i = 1; i <= query.n_params; i++) {
+    g_value_init (&args[i], query.param_types[i]);
+
+    GST_ERROR_OBJECT (action, "param_type: %s",
+        g_type_name (query.param_types[0]));
+
+    if (query.param_types[i - 1] == G_TYPE_STRING) {
+      g_value_set_string (&args[i], arg_list[i]);
+    } else if (query.param_types[i - 1] == G_TYPE_INT) {
+      g_value_set_int (&args[i], atoi (arg_list[i]));
+    } else if (query.param_types[i - 1] == G_TYPE_UINT) {
+      g_value_set_uint (&args[i], (guint) atoi (arg_list[i]));
+    } else if (query.param_types[i - 1] == G_TYPE_UINT64) {
+      g_value_set_uint64 (&args[i], (guint64) g_ascii_strtoull (arg_list[i],
+              NULL, 10));
+      GST_ERROR_OBJECT (action, "arg_list[i+1]: %s", arg_list[i]);
+      GST_ERROR_OBJECT (action, "value: %ld", g_value_get_uint64 (&args[i]));
+    } else if (query.param_types[i - 1] == G_TYPE_BOOLEAN) {
+      g_value_set_boolean (&args[i], g_strcmp0 (arg_list[i], "true") == 0);
+    } else if (query.param_types[i - 1] == G_TYPE_FLOAT) {
+      g_value_set_float (&args[i], g_ascii_strtod (arg_list[i], NULL));
+    } else if (query.param_types[i - 1] == G_TYPE_DOUBLE) {
+      g_value_set_double (&args[i], g_ascii_strtod (arg_list[i], NULL));
+    } else {
+      ret = GSTD_BAD_VALUE;
+      goto out;
+    }
   }
+
+  // Set return value container as G_TYPE_INT, stored in ret_sig
+  g_value_init (&args[query.n_params], G_TYPE_INT);
+
+  GST_ERROR_OBJECT (action, "Emit to %s", GST_OBJECT_NAME (action->target));
+  GST_ERROR_OBJECT (action, "description: %s", description);
+  GST_ERROR_OBJECT (action, "name: %s", name);
+  GST_ERROR_OBJECT (action, "n_params: %d", query.n_params);
+  GST_ERROR_OBJECT (action, "object name(action): %s",
+      GSTD_OBJECT_NAME (action));
+
+  g_signal_emitv (args, action_id, 0, &args[query.n_params]);
+
+  ret_sig = g_value_get_int (&args[query.n_params]);
+
+  if (ret_sig != 0)
+    ret = GSTD_BAD_VALUE;
 
 out:
+  for (guint i = 0; i <= query.n_params; i++)
+    g_value_unset (&args[i]);
+  g_free (args);
+  g_strfreev (arg_list);
+
   return ret;
 }
